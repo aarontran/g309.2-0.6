edcb2a4085049db9df924e17a35c3632889f3696
Aaron Tran <atran@cfa.hvd>
2016-07-08 15:25:31 -0400

    Combine annulus/src region plots
    Highlights src region, includes background.  More compact display.

--- xmm/make_ms_image.py
+++ xmm/make_ms_image.py
@@ -78,33 +78,12 @@ def main(fig_num):
                              figsize=(7,5))  # only height affects final size
         f.recenter(ra2deg(13,46,30), -62.9, width=30./60, height=24./60)
         f.show_colorscale(vmin=1e-5, stretch='log',cmap='hot')
-        f.show_regions('regs/bkg.reg')
-        f.show_regions('regs/src.reg')
-
-        most = fits.open(XMMPATH + '/../most/G309.2-0.6.fits')
-        #lev = np.linspace(0.02, 0.2, 10)  # Lin MOST contours
-        lev = np.logspace(np.log10(0.01), np.log10(0.2), 10)  # Log MOST contours
-        #lev = [0.01, 0.01*np.sqrt(10), 0.1, 0.1*np.sqrt(10)]  # Sparse MOST contours
-        f.show_contour(data=most, levels=lev, colors='lime', alpha=0.5)
-
-        f.tick_labels.set_yformat('dd:mm')
-        f.tick_labels.set_xformat('hh:mm')
-        f.axis_labels.set_ypad(-5)
-        f.refresh()
-        f.image.figure.tight_layout()  # Use the matplotlib figure instance
-        f.save('fig_snr_regs-src-bkg.pdf', dpi=300)  # Drops into CWD
-
-    elif fig_num == '3':
-
-        f = aplpy.FITSFigure(XMMPATH + '/results_img/merged-im-sky-0.8-3.3kev-test.fits',
-                             figsize=(7,5))  # only height affects final size
-        f.recenter(ra2deg(13,46,30), -62.9, width=30./60, height=24./60)
-        f.show_colorscale(vmin=1e-5, stretch='log',cmap='hot')
-        f.show_regions('regs/ann_000_100.reg')
-        f.show_regions('regs/ann_100_200.reg')
-        f.show_regions('regs/ann_200_300.reg')
-        f.show_regions('regs/ann_300_400.reg')
-        f.show_regions('regs/ann_400_500.reg')
+        f.show_regions('regs/circ_100.reg')
+        f.show_regions('regs/circ_200.reg')
+        f.show_regions('regs/circ_300.reg')
+        f.show_regions('regs/src.reg')  # Equivalent to circ_400, but changed color/width
+        f.show_regions('regs/circ_500.reg')
+        f.show_regions('regs/bkg.reg')  # Yes, do show background
 
         most = fits.open(XMMPATH + '/../most/G309.2-0.6.fits')
         #lev = np.linspace(0.02, 0.2, 10)  # Lin MOST contours
@@ -118,6 +97,8 @@ def main(fig_num):
         f.refresh()
         f.image.figure.tight_layout()  # Use the matplotlib figure instance
         f.save('fig_snr_regs-fiveann.pdf', dpi=300)  # Drops into CWD
+    else:
+        raise Exception("Invalid figure number")
 
 
 def ra2deg(h,m,s):
@@ -126,6 +107,5 @@ def ra2deg(h,m,s):
 
 
 if __name__ == '__main__':
-    main('1')
+    #main('1')
     main('2')
-    main('3')
