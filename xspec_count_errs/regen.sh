e32200aca500c96eec6f1b5642454909b75d324c
Aaron Tran <atran@cfa.hvd>
2016-08-19 18:55:46 -0400

    Short writeup on xspec counting errors

---
+++ xspec_count_errs/regen.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+fname="xspec_count_errs"
+
+# Run bibtex if any command line arg given (gibberish OK)
+if [[ "$#" -eq 1 ]]; then
+  pdflatex "${fname}.tex" 
+  bibtex $fname
+fi
+pdflatex "${fname}.tex"
+pdflatex "${fname}.tex"
+
+# Only "deploy" on HEAD computer
+if [[ $HOSTNAME == 'treble' ]]; then
+  echo
+  echo "cp ${fname}.pdf /data/wdocs/atran/g309/."
+  cp "${fname}.pdf" /data/wdocs/atran/g309/.
+fi
+
+
